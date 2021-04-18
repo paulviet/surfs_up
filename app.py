67eb2b28f55cdfaa33c987a286d88bf7127039fd
paulviet <9637489+paulviet@users.noreply.github.com>
2021-04-18 02:38:59 -0400

    9-5-6 Flask Alchemy ORM Statistics Route

--- app.py
+++ app.py
@@ -1,11 +1,108 @@
 # Dependencies
-from flask import Flask
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+# Dependencies from sqlalchemy
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+# Dependencies flask
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///hawaii.sqlite")
+#?check_same_thread=False
+# notes on check_same_thread https://stackoverflow.com/questions/34009296/using-sqlalchemy-session-from-flask-raises-sqlite-objects-created-in-a-thread-c
+# added session.close() at end of @app.routes because of errors below
+# sqlalchemy.exc.ProgrammingError: (sqlite3.ProgrammingError) SQLite objects created in a thread can only be used in that same thread. The object was created in thread id 11268 and this is thread id 20672.
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # create instance of Flask app
 app = Flask(__name__)
 
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+
 # Create route that renders index.html template
 # and takes in the static string "Serving up cool text from the Flask server!"
 @app.route("/")
-def hello_world():
-    return 'Hello world'
+def welcome():
+
+    return(
+    '''
+    Welcome to the Climate Analysis API!<br/>
+    Available Routes:<br/>
+    /api/v1.0/precipitation<br/>
+    /api/v1.0/stations<br/>
+    /api/v1.0/tobs<br/>
+    /api/v1.0/temp/start/end<br/>
+    ''')
+
+# date and precipitation for the previous year.
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   session.close()
+   return jsonify(precip)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.station).all()
+    #unraveled results into a list
+    stations = list(np.ravel(results))
+    session.close()
+    return jsonify(stations=stations)
+
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).\
+      filter(Measurement.station == 'USC00519281').\
+      filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    session.close()
+    return jsonify(temps=temps)
+
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).\
+            filter(Measurement.date <= end).all()
+        temps = list(np.ravel(results))
+        session.close()
+        return jsonify(temps)
+
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    session.close()
+    return jsonify(temps=temps)
+
+session.close()
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
